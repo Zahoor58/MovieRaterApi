51cc405d4e0476affd7b283857e800cb845021dc
zahoor ahmad <86906265+Zahoor58@users.noreply.github.com>
2022-09-28 02:32:27 +0500

    Explored custom method and request data and update and create

--- movierater/api/views.py
+++ movierater/api/views.py
@@ -1,11 +1,40 @@
+from urllib import response
 from django.shortcuts import render
-from rest_framework import  viewsets
+from rest_framework import  viewsets,status
 from .models import Movie, Rating
+from  django.contrib.auth.models import User
+from  rest_framework.response import Response
+from rest_framework.decorators import action
 # Create your views here.
 from  . serializers import  MovieSerializer, RatingSerializer
 class  MovieViewSet(viewsets.ModelViewSet):
     queryset=Movie.objects.all()
     serializer_class=MovieSerializer
+    
+    @action(detail=True,methods=['POST'])
+    def rate_movie(self,request,pk=None):
+        if 'stars'  in request.data:
+            movie=Movie.objects.get(id=pk)
+            stars=request.data['stars']
+            # user=request.user
+            user=User.objects.get(id=1)
+            print('user',user.username)
+            print('movie title', movie.title)
+            try:
+                rating=Rating.objects.get(user=user.id, movie=movie.id)
+                rating.stars=stars
+                rating.save()
+                serializer=RatingSerializer(rating,many=False)
+                response={"message":"Rating  Updated", 'result':serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                 rating=Rating.objects.create(user=user, movie=movie,stars=stars)
+                 serializer=RatingSerializer(rating,many=False)
+                 response={"message":"Rating Created", 'result':serializer.data}
+                 return Response(response, status=status.HTTP_200_OK)
+        else: 
+            response={"message":"You need to provide stars"}
+            return Response(response, status=status.HTTP_200_OK)
 
 class  RatingViewSet(viewsets.ModelViewSet):
     queryset=Rating.objects.all()
